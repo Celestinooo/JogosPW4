b3b8bc502e7edd9b2918eccf8646212fac74b422
Ricardo_Teixeira <libra1ponciano@gmail.com>
2022-11-26 10:58:43 -0300

    empresa

---
+++ jogos-pw4/src/components/Empresa/index.jsx
@@ -0,0 +1,55 @@
+import { StyleEmpresa } from './style.jsx';
+import {useState, useEffect} from 'react';
+
+export default function Jogo() {
+    const retornoUseState =  useState([]);
+    const state = retornoUseState[0];
+    const setState = retornoUseState[1];
+
+    const [erro, setErro] = useState({
+        hasErro: false,
+        mensagemErro: ""
+    })
+
+    async function buscaEmpresa() {
+        const res = await fetch('http://localhost:8888/empresa/buscar').catch((erro) =>{
+            //tratamento de erro
+            console.log("Erro ao realizar o fetch");
+            setErro({
+                hasErro: true,
+                mensagemErro: "Erro ao realizar a busca. Verifique o servidor"
+            })
+        })
+
+        if(res.status >= 200 && res.status <= 299){
+            const resJson = await res.json();
+            setState(resJson);
+            
+        }else{
+            console.log(`Erro! Requisição com código ${res.status}`)
+        }
+    }
+    
+    async function criaEmpresa(form) {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 
+                nome: form.nome,
+                descricao: form.descricao,
+                avaliacao: form.avaliacao,
+                anoLancamento: form.anoLancamento,
+                empresaId: form.empresaId,
+            })
+        };
+        fetch("http://localhost:8888/empresa/criar", requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
+    }
+
+    return (
+        <StyleJogo>
+            <h1>TESTE DA EMPRESA</h1>
+        </StyleJogo>
+    );
+}
---
+++ jogos-pw4/src/components/Empresa/style.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyleEmpresa = styled.div`
+    display: flex;
+    justify-content: center;
+    h1 {
+        font-size: 80px;
+    }
+`;
