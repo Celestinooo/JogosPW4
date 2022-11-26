bc60cb5de2ad1ba07e544f750052843fff95db78
Ricardo_Teixeira <libra1ponciano@gmail.com>
2022-11-26 10:56:22 -0300

    function cria jogo

--- jogos-pw4/src/components/Jogo/index.jsx
+++ jogos-pw4/src/components/Jogo/index.jsx
@@ -10,28 +10,42 @@ export default function Jogo() {
         hasErro: false,
         mensagemErro: ""
     })
-    useEffect(() => {
-        async function buscaJogo() {
-            const res = await fetch('http://localhost:8888/jogo/buscar').catch((erro) =>{
-                //tratamento de erro
-                console.log("Erro ao realizar o fetch");
-                setErro({
-                    hasErro: true,
-                    mensagemErro: "Erro ao realizar a busca. Verifique o servidor"
-                })
-            })
 
-            if(res.status >= 200 && res.status <= 299){
-                const resJson = await res.json();
-                setState(resJson);
-                
-            }else{
-                console.log(`Erro! Requisição com código ${res.status}`)
-            }
+    async function buscaJogo() {
+        const res = await fetch('http://localhost:8888/jogo/buscar').catch((erro) =>{
+            //tratamento de erro
+            console.log("Erro ao realizar o fetch");
+            setErro({
+                hasErro: true,
+                mensagemErro: "Erro ao realizar a busca. Verifique o servidor"
+            })
+        })
+
+        if(res.status >= 200 && res.status <= 299){
+            const resJson = await res.json();
+            setState(resJson);
+            
+        }else{
+            console.log(`Erro! Requisição com código ${res.status}`)
         }
-        
-        buscaJogo();
-    }, []);
+    }
+    
+    async function criaJogo(form) {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ 
+                nome: form.nome,
+                descricao: form.descricao,
+                avaliacao: form.avaliacao,
+                anoLancamento: form.anoLancamento,
+                empresaId: form.empresaId,
+            })
+        };
+        fetch("http://localhost:8888/jogo/criar", requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
+    }
 
     return (
         <StyleJogo>
